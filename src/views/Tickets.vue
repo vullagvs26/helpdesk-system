248ec7147934c777cae1cd263123a9df3760ad3d
vullagvs26 <vincentvullag@gmail.com>
2024-07-15 10:31:47 +0800

    Display Tickets and fixing search tickets

--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrashAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -7,42 +7,38 @@ import { faTrashAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
 // Importing images
 import profileImage from "@/assets/image/profile.png";
 import ticketImage from "@/assets/image/fujitsu-logo.png";
+import { useDeveloperStore } from "@/modules/developer.js";
+import { useTicketStore } from "@/modules/ticket.js";
+import { useSystemStore } from "@/modules/system.js";
 
 library.add(faTrashAlt, faSearch);
 
-//sample data
-const developers = [
-  { name: "You", status: "Available", image: profileImage },
-  { name: "Reina", status: "Available", image: profileImage },
-  { name: "Gerly", status: "Available", image: profileImage },
-  { name: "Gian", status: "Busy", image: profileImage },
-];
-
-const tickets = ref([
-  {
-    id: 1,
-    title: "PQH Concern - Mamaril, Andrea",
-    description: "May duplicate po kami nakita sa sorting request.",
-    status: "Open",
-    image: ticketImage,
-  },
-  {
-    id: 2,
-    title: "PDLSv4 Concern - Montañez, Michelle",
-    description: "Sir Mico, Please fix my broken heart",
-    status: "Open",
-    image: ticketImage,
-  },
-]);
+const developerStore = useDeveloperStore();
+const ticketStore = useTicketStore();
+const systemStore = useSystemStore();
+const developers = ref(null);
+const tickets = ref([]);
+const systems = ref(null);
 
 //filter
 const searchQuery = ref("");
 
-const filteredTickets = computed(() => {
+const ticketsSearch = computed(() => {
   return tickets.value.filter((ticket) =>
-    ticket.title.toLowerCase().includes(searchQuery.value.toLowerCase())
+    ticket.ticket_no.toLowerCase().includes(searchQuery.value.toLowerCase())
   );
 });
+
+const fetchDevelopersAndTickets = () => {
+  developerStore.setLoadDeveloper().then(() => {
+    developers.value = developerStore.getLoadDeveloper;
+  });
+  ticketStore.setLoadTicket().then(() => {
+    tickets.value = ticketStore.getLoadTicket;
+  });
+};
+
+onMounted(fetchDevelopersAndTickets);
 </script>
 
 <template>
@@ -54,15 +50,15 @@ const filteredTickets = computed(() => {
       <div class="flex space-x-4">
         <div
           v-for="(developer, index) in developers"
-          :key="index"
+          :key="developer.id"
           class="flex flex-col items-center bg-white shadow-md rounded-lg p-4"
         >
           <img
-            :src="developer.image"
+            :src="developer.profile_photo"
             alt="Developer Image"
             class="w-16 h-16 rounded-full mb-2"
           />
-          <p class="font-medium text-center">{{ developer.name }}</p>
+          <p class="font-medium text-center">{{ developer.first_name }}</p>
           <p
             :class="{
               'text-green-500': developer.status === 'Available',
@@ -85,23 +81,23 @@ const filteredTickets = computed(() => {
           placeholder="Search"
           class="border rounded-full pl-4 pr-10 py-1"
         />
-        <font-awesome-icon
-          icon="search"
-          class="absolute right-3 top-2 text-gray-400"
-        />
+        <font-awesome-icon icon="search" class="absolute right-3 top-2 text-gray-400" />
       </div>
     </div>
 
     <div>
       <div
-        v-for="ticket in filteredTickets"
+        v-for="ticket in ticketsSearch"
         :key="ticket.id"
         class="mb-4 p-4 bg-white border border-gray-200 rounded-lg flex justify-between items-center"
       >
         <div class="flex items-center">
           <img :src="ticket.image" alt="Ticket Image" class="w-12 h-12 mr-4" />
           <div>
-            <h4 class="font-semibold">{{ ticket.title }}</h4>
+            <h4 class="font-semibold">
+              {{ ticket.ticket_no }} | {{ ticket.system.system_name }} |
+              {{ ticket.full_name }}
+            </h4>
             <p class="text-gray-600">{{ ticket.description }}</p>
             <button class="text-blue-500 mt-2">Open</button>
           </div>
